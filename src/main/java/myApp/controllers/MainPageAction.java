b1ab741df268071f5bcf5fdae375f326b8b68873
Занин Дмитрий Владимирович <OUT-Zanin-DV@sigma.sbrf.ru>
2019-11-06 17:19:16 +0300

    read all csv files from folder

--- src/main/java/myApp/controllers/MainPageAction.java
+++ src/main/java/myApp/controllers/MainPageAction.java
@@ -1,16 +1,12 @@
 package myApp.controllers;
 
-import myApp.model.DictionaryEntity;
-import myApp.utils.HibernateUtil;
+import myApp.model.DataPacket;
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.hibernate.Session;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 public class MainPageAction extends Action {
 
@@ -18,15 +14,8 @@ public class MainPageAction extends Action {
     public ActionForward execute(ActionMapping mapping,
                                  ActionForm form,
                                  HttpServletRequest request,
-                                 HttpServletResponse response)
-    {
-        final Session session = HibernateUtil.getHibernateSession();
-        session.beginTransaction();
-        List tt = session.createSQLQuery("select * from TEST.T_DICTIONARY limit 1")
-                .addEntity(DictionaryEntity.class)
-                .list();
-        System.out.println(tt);
-        session.close();
-        return mapping.findForward("main");
+                                 HttpServletResponse response) throws Exception {
+        DataPacket.readFiles();
+       return mapping.findForward("main");
     }
 }
--- src/main/java/myApp/model/DataPacket.java
+++ src/main/java/myApp/model/DataPacket.java
@@ -1,10 +1,64 @@
 package myApp.model;
 
-import org.hibernate.Session;
+import org.apache.commons.io.FilenameUtils;
+
+import javax.activation.MimetypesFileTypeMap;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Objects;
 
 public class DataPacket {
-    public void sendPacket(Session session)
-    {
-//        if (session.isDirty())
+    static {
+        try {
+            Class.forName("org.h2.Driver");
+            String url = "jdbc:h2:tcp://localhost/~/test";
+            connection = DriverManager.getConnection(url, "sa", "sa");
+        }
+        catch (Exception ex) {
+            throw new Error();
+        }
+    }
+
+    private static Connection connection;
+    private static final int BUFFSIZE = 10;
+
+    public Connection getConnection() { return connection; }
+
+    //метод проверяет все файлы в заданной папке и загружает их в бд, если их тип csv
+    public static void readFiles() throws Exception {
+        final File folder = new File(Configuration.getInputDirectory());
+
+        for (final File file : Objects.requireNonNull(folder.listFiles())) {
+            if (FilenameUtils.getExtension(file.getName()).equals("csv")) {
+                FileReader fileReader = new FileReader(file);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+                while (sendPacket(bufferedReader)) {
+                    System.out.println("package read!");//need log!
+                }
+            } else {
+                System.out.println("invalid file!!");//NEED LOG !!
+            }
+        }
+    }
+
+    //читывает из файлового дескриптора BUFFSIZE строк, создает sql запрос на insert и выполняет его
+    private static boolean sendPacket(BufferedReader fileReader) throws Exception {
+        Statement stmt;//запрос
+        String buffer;
+        stmt = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);//создаю запрос
+        connection.setAutoCommit(false);//вырубаю автокоммит
+        //stmt.addBatch("INSERT INTO TEST.T_DICTIONARY(ID, NAME, VALUE) VALUES('?', '?', '?')");
+        for (int i = 0; i < BUFFSIZE && (buffer = fileReader.readLine()) != null; i++) {
+            System.out.println(buffer);
+        }
+        stmt.executeBatch();
+        connection.commit();
+        stmt.close();
+        return false;
     }
 }
